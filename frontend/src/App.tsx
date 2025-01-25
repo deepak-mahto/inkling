33ba931fa976d3ad59525fbedeebbd5de4815257
Deepak Kumar <mahtodeepak857@gmail.com>
2025-01-25 20:54:13 +0530

    Basic routing in fe

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Blog from "./pages/Blog";
+
 const App = () => {
   return (
-    <div className="flex justify-center items-center text-9xl h-screen w-full underline">
-      Inkling
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/blog/:id" element={<Blog />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 };
 export default App;
