c6e475797d2fe2acef3f2f0ef4078e360d41a74b
Deepak Kumar <mahtodeepak857@gmail.com>
2025-01-25 15:56:48 +0530

    Implemented basic routes

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -19,9 +19,9 @@ userRouter.post("/signup", async (c) => {
   try {
     const user = await prisma.user.create({
       data: {
+        name: body.name,
         email: body.email,
         password: body.password,
-        name: body.name,
       },
     });
 
